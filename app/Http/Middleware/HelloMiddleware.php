a2c4b72374c11528c28d07b7b765e1e7168fb054
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-01-24 11:39:58 +0900

    リスト4-2

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -8,6 +8,12 @@ class HelloMiddleware
 {
     public function handle($request, Closure $next)
     {
+        $data = [
+            ['name'=>'taro', 'mail'=>'taro@yamada'],
+            ['name'=>'hanako', 'mail'=>'hanako@flower'],
+            ['name'=>'sachiko', 'mail'=>'sachiko@happy'],
+        ];
+        $request->marge(['data'=>$data]);
         return $next($request);
     }
 }
