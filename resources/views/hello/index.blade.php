8d09ec0a7b93d32633c53099074d307f19b4d8dc
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-01-26 12:43:46 +0900

    リスト4-37

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,45 +9,28 @@
 
 @section('content')
     <p>{{ $msg }}</p>
-    @if (count($errors) > 0)
-        <p>入力に問題があります。再入力して下さい。</p>
+    @if(count($errors) > 0)
+    <p>入力に問題があります。再入力してください。</p>
     @endif
-    <form action="/hello" method="post">
-    <table>
-        @error('name')
+    <form action="hello" method="post">
+        <table>
+            @csrf
+            @if ($errors->has('msg'))
             <tr>
                 <th>ERROR</th>
-                <td>{{ $message }}</td>
+                <td>{{ $errors->first('msg') }}</td>
             </tr>
-        @enderror
-        <tr>
-            <th>name:</th>
-            <td><input type="text" name="name" value="{{ old('name') }}"></td>
-        </tr>
-        @error('mail')
-        <tr>
-            <th>ERROR</th>
-            <td>{{ $message }}</td>
-        </tr>
-        @enderror
-        <tr>
-            <th>mail:</th>
-            <td><input type="text" name="mail" value="{{ old('mail') }}"></td>
-        </tr>
-        @error('age')
-        <tr>
-            <th>ERROR</th>
-            <td>{{ $message }}</td>
-        </tr>
-        @enderror
-        <tr>
-            <th>age:</th>
-            <td><input type="text" name="age" value="{{ old('age') }}"></td>
-        </tr>
-        <tr>
-            <th></th>
-            <td><input type="submit" value="send"></td>
-        </tr>
+            @endif
+            <tr>
+                <th>Message:</th>
+                <td><input type="text" name="msg" value="{{ old('msg') }}"></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="send"></td>
+            </tr>
+        </table>
+    </form>
 @endsection
 
 @section('footer')
