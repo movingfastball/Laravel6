d2fc0ef579b4cf3c90e965b54ba6c714751d4452
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-01-27 11:31:47 +0900

    リスト5-4

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,21 +6,18 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Requests\HelloRequest;
 use Validator;
+use Illuminate\Support\Facades\DB;
+
+
 class HelloController extends Controller
 {
 
     public function index(Request $request) 
     { 
-        if ($request->hasCookie('msg'))
-        {
-            $msg = 'Cookie:' . $request->cookie('msg');
-        } else {
-            $msg = '※クッキーではありません';
-        }
-
-        return view('hello.index',['msg'=> $msg]);
+        $items = DB::select('select * from people');
+        return view('hello.index',['items'=> $items]);
     }
-
+/*
     public function post(Request $request)
     {
         $validate_rule = [
@@ -32,4 +29,5 @@ class HelloController extends Controller
         $response->cookie('msg', $msg, 100);
         return $response;
     }
+ */   
 }
