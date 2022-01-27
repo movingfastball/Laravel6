23a9f32f57f58389ec0e0348a2aee990fdde80de
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-01-27 15:04:37 +0900

    リスト5-10

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::get('/', function () {
 
 Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
+Route::get('hello/add', 'HelloController@add');
+Route::post('hello/add', 'HelloController@create');
