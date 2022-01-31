3416c84a98df94838113fe3eb8189f4b0b35a94a
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-01-31 11:55:43 +0900

    リスト5-15

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -58,4 +58,18 @@ class HelloController extends Controller
 
         return redirect('/hello');
     }
+
+    public function del(Request $request)
+    {
+        $param = ['id' => $request->id];
+        $item = DB::select('SELECT * FROM people WHERE id = :id', $param);
+        return view('hello.del', ['form' => $item[0]]);
+    }
+
+    public function remove(Request $request)
+    {
+        $param = ['id' => $request->id];
+        DB::delete('DELETE FROM people WHERE id = :id', $param);
+        return redirect('/hello');
+    }
 }
