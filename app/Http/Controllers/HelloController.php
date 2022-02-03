3d8b6d122c34a5d6d7dbd006e7f5a077a07cdbf5
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-02-03 15:11:38 +0900

    リスト5-23_fix

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -75,7 +75,7 @@ class HelloController extends Controller
 
     public function show(Request $request)
     {
-        $id = $request->name;
+        $name = $request->name;
         $items = DB::table('people')
             ->where('name', 'like', '%' . $name . '%')
             ->orWhere('mail', 'like', '%' . $name . '%')
