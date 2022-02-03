85c024414d0c967d97ec5be7e4c012a6a4db6a23
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-02-03 16:08:13 +0900

    リスト5-28

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -41,9 +41,10 @@ class HelloController extends Controller
 
     public function edit(Request $request)
     {
-        $param = ['id' => $request->id];
-        $item = DB::select('select * from people where id = :id', $param);
-        return view('hello.edit', ['form' => $item[0]]);
+        $item = DB::table('people')
+            ->where('id', $request->id)
+            ->first();
+        return view('hello.edit', ['form' => $item]);
     }
 
     public function update(Request $request)
@@ -54,7 +55,9 @@ class HelloController extends Controller
             'mail' => $request->mail,
             'age' => $request->age,
         ];
-        DB::update('UPDATE people SET name = :name, mail = :mail, age = :age WHERE id = :id', $param );
+        DB::table('people')
+            ->where('id', $request->id)
+            ->update($param);
 
         return redirect('/hello');
     }
