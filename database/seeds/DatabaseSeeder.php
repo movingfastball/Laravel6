160be05f1255fc5032ec38befa5477e924dddc80
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-02-03 18:09:44 +0900

    peopleテーブルが通らないので一時対応

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,6 +6,25 @@ class DatabaseSeeder extends Seeder
 {
    public function run()
    {
-       $this->call(PeopleTableSeeder::class); //●
+    $param = [
+        'name' => 'taro',
+        'mail' => 'taro@yamada.jp',
+        'age' => 12,
+    ];
+    DB::table('people')->insert($param);
+
+    $param = [
+        'name' => 'hanako',
+        'mail' => 'hanako@flower.jp',
+        'age' => 34,
+    ];
+    DB::table('people')->insert($param);
+
+    $param = [
+        'name' => 'sachiko',
+        'mail' => 'sachiko@happy.jp',
+        'age' => 56,
+    ];
+    DB::table('people')->insert($param);
    }
 }
