b10461b06c9b0e2cc2ca4eb3dd332505fa95c956
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-02-03 16:34:08 +0900

    リスト5-29

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -64,15 +64,16 @@ class HelloController extends Controller
 
     public function del(Request $request)
     {
-        $param = ['id' => $request->id];
-        $item = DB::select('SELECT * FROM people WHERE id = :id', $param);
-        return view('hello.del', ['form' => $item[0]]);
+        $item = DB::table('people')
+            ->where('id', $request->id)
+            ->first();
+        return view('hello.del', ['form' => $item]);
     }
 
     public function remove(Request $request)
-    {
-        $param = ['id' => $request->id];
-        DB::delete('DELETE FROM people WHERE id = :id', $param);
+    {       
+        DB::table('people')
+            ->where('id', $request->id)->delete();
         return redirect('/hello');
     }
 
