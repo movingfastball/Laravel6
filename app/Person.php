18c41380524cd1cde8b4d2cd073483ebe56a2aae
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-02-06 15:17:20 +0900

    6-35

--- app/Person.php
+++ app/Person.php
@@ -42,7 +42,7 @@ class Person extends Model
         static::addGlobalScope(new ScopePerson);
     }
 
-    public function board()
+    public function boards()
     {
         return $this->hasMany('App\Board');
     }
