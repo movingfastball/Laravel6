abdbc381eca600b4563648dd80e15a1aeb28f9e3
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-02-07 12:40:30 +0900

    シーダー回避

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Restdata;
+use App\Person;
 
 class DatabaseSeeder extends Seeder
 {
@@ -26,5 +28,24 @@ class DatabaseSeeder extends Seeder
         'age' => 56,
     ];
     DB::table('people')->insert($param);
+
+    $param = [
+        'message' => 'Google Japan',
+        'url' => 'https://www.google.co.jp',
+    ];
+    $restdata = new Restdata;
+    $restdata->fill($param)->save();
+    $param = [
+        'message' => 'Yahoo Japan',
+        'url' => 'https://www.yahoo.co.jp'
+    ];
+    $restdata = new Restdata;
+    $restdata->fill($param)->save();
+    $param = [
+        'message' =>'MSN Japan',
+        'url' => 'http://www.msn.com/ja-jp',
+    ];
+    $restdata = new Restdata;
+    $restdata->fill($param)->save();
    }
 }
