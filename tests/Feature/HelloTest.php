78cff27b71031cded88699c92afd602d4a8c9ce5
movingfastball <movingfastballny2012@yahoo.co.jp>
2022-02-28 12:44:25 +0900

    7-40

---
+++ tests/Feature/HelloTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+/*
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+*/
+/*
+class HelloTest extends TestCase
+{
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
+*/
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class HelloTest extends TestCase
+{
+
+    public function testHello()
+    {
+        $test->assertTrue(true);
+
+        $arr = [];
+        $this->assertEmpty($arr);
+
+        $msg = "Hello";
+        $this->assertEquals('Hello',$msg);
+
+        $n = random_int(0, 100);
+        $this->assertLessThan(100, $n);
+    }
+}
+
+
+
+
